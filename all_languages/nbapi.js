4b6125c406a12677056a987cb558552cbe5ad152
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2019-10-03 11:41:08 -0700

    Update nbapi.js

--- all_languages/nbapi.js
+++ all_languages/nbapi.js
@@ -1,1 +1,24 @@
 //WILL WORK ON IN THE NEXT HOUR (10:53 AM)
+
+
+//i don't know if this will work, i doubt it does /shrug
+exports.RandomNeko = function() {
+const https = require('https');
+
+https.get('http://neko-bot.net/info/totalnekos.txt', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    return (JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+}
