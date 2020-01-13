4f98f7550d9b6a2775219e557f332a5843c4959e
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2020-01-13 12:18:00 -0800

    Add files via upload

---
+++ versions/raw/nbapi-1.9.0.1.4/lib/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+from textwrap import wrap
+def NewVersion():
+    f = open("nbapi/version.txt","r+")
+    v1 = str(f.read()).replace(".","")
+    temp = str(int(v1) + 1)
+    temp1 = wrap(temp,1)
+    print(temp1)
+    out = ".".join(temp1)
+    f.seek(0)
+    f.truncate()
+    f.write(out)
+    f.close()
+    print(out)
+    
+    
+
+
+
+NewVersion()
+long_description = "Simple Anime API package"
+versionF=open("nbapi/version.txt","r").read()
+setuptools.setup(
+    name="nbapi",
+    version=versionF,
+    author="LazyNeko",
+    author_email="nekobot.help@gmail.com",
+    description="A small API for anime/nekos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LazyNeko1/nbapi",
+    #packages="nbapi",
+    install_requires=['aiohttp','requests','asyncio'],
+    package_data={
+        'nbapi':['*.py','version.txt']
+    },
+    py_modules=['nbapi/__init__'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
